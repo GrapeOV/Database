632636192a989d682254d83b0fa947a314b52c10
Dmitriy Vinogradov <98027619+GrapeOV@users.noreply.github.com>
2022-03-29 02:17:34 +0300

    Update Task 5.sql

--- Task 5.sql
+++ Task 5.sql
@@ -1,7 +1,7 @@
 --[3.5 Пункт] Показать список складов (LocationID), на которых содержатся  несколько продуктов (ProductID) из таблицы Production.ProductInventory
 USE AdventureWorks2019
 
-SELECT DISTINCT (LocationID)
-FROM (SELECT LocationID, ProductID, sum(Quantity) as cnt from Production.ProductInventory
-group by LocationID, ProductID) a
-where cnt>1
+SELECT DISTINCT a.LocationID, a.ProductID
+FROM Production.ProductInventory as a
+join Production.ProductInventory as b on a.LocationID = b.LocationID
+and a.ProductID <> b.ProductID
