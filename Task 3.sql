b64432f2dd3009ca9425bc25e6482dc7bccec884
Dmitriy Vinogradov <98027619+GrapeOV@users.noreply.github.com>
2022-03-29 02:20:05 +0300

    Update Task 3.sql

--- Task 3.sql
+++ Task 3.sql
@@ -1,7 +1,14 @@
 --[4.3 Пункт] Показать товар, для которого существует более 2 цветов в той же подкатегории (Таблица Production.Product). Показать поля [Name], COLOR и ProductSubcategoryID
 USE AdventureWorks2019
 
-select ProductSubcategoryID, Color, count(*) as cnt 
-from Production.Product
-where not ProductSubcategoryID is null and not Color is null
-group by ProductSubcategoryID, Color
+SELECT p1.[Name], p1.Color, p1.ProductSubcategoryID
+FROM Production.Product as p1
+WHERE p1.Color IS NOT NULL
+AND p1.ProductSubcategoryID IS NOT NULL
+AND (
+SELECT COUNT(DISTINCT p2.Color) as ColorCount
+FROM Production.Product as p2
+WHERE p2.color IS NOT NULL
+AND p1.ProductSubcategoryID = p2.ProductSubcategoryID
+) > 2
+ORDER BY p1.Name, p1.ProductSubcategoryID
