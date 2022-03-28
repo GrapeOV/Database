8ad297656e321bff671aa7cb4123b57cd2cf9424
Dmitriy Vinogradov <98027619+GrapeOV@users.noreply.github.com>
2022-03-29 02:18:35 +0300

    Update Task 8.sql

--- Task 8.sql
+++ Task 8.sql
@@ -1,5 +1,8 @@
 --[3.8 Пункт] Показать список ID которые содержатся в таблице Sales.Store(поле BusinessEntityID), но не содержатся в таблицах Sales.Customer(поле CustomerID)
 USE AdventureWorks2019
 
-select BusinessEntityID from Sales.Store
-where not BusinessEntityID in (select CustomerId from Sales.Customer)
+SELECT BusinessEntityID
+FROM Sales.Store
+EXCEPT
+SELECT CustomerID
+FROM Sales.Customer
